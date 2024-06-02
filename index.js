567c18bb3d9a63de7ade61fb7724d56a7b238e30
melo4yxa77796 <g_julija_777@mail.ru>
2024-06-02 22:33:12 +0200

    add link to js file

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Hello world");
+let yearOfBirth = 1989;
+let yearFuture=2027;
+let age = yearFuture - yearOfBirth;
+console.log(`You will be  + ${age} +  years old in + ${yearFuture} + .`);

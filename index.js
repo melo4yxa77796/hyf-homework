712aae7394548f3663613242b2e0b0d90e6dbad0
melo4yxa77796 <g_julija_777@mail.ru>
2024-06-10 12:38:01 +0200

    first exercise

---
+++ index.js
@@ -0,0 +1,14 @@
+function getFullname(firstname,surname){
+  return firstname + " " + surname;
+ 
+}
+
+console.log(getFullname("Benjamin","Hughes"));
+
+let fullname1 = getFullname("Benjamin", "Hughes");
+let fullname2 = getFullname("Emma", "Smith");
+
+console.log(fullname1); // returns "Benjamin Hughes"
+console.log(fullname2); // returns "Emma Smith"
+
+

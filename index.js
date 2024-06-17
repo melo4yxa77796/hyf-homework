043765b325a45e086edfb2b510559efec6955d8c
melo4yxa77796 <g_julija_777@mail.ru>
2024-06-17 10:17:45 +0200

    finished "When will we be there?"

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ if (index !== -1) {
 // Code done
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+//When will we be there??
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+function calculateTravelTime(travelInformation) {
+  const speed = travelInformation.speed;
+  const destination = travelInformation.destinationDistance;
+  let time = destination / speed;
+  const hours = Math.floor(time);
+  const minutes = Math.round((time - hours) * 60);
+  return `${hours} hours and ${minutes} minutes`;
+}
+
+const travelTime = calculateTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes

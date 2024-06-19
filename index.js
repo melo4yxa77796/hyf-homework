35aceeb1d6d63ce3cb22e9f9625eed483c0e994c
melo4yxa77796 <g_julija_777@mail.ru>
2024-06-19 21:30:03 +0200

    finished Series duration of my life

--- index.js
+++ index.js
@@ -37,3 +37,52 @@ function calculateTravelTime(travelInformation) {
 
 const travelTime = calculateTravelTime(travelInformation);
 console.log(travelTime); // 8 hours and 38 minutes
+
+//Series duration of my life
+const seriesDurations = [
+  {
+    title: "Game of Thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "The Sopranos",
+    days: 3,
+    hours: 14,
+    minutes: 0,
+  },
+  {
+    title: "The Wire",
+    days: 2,
+    hours: 12,
+    minutes: 0,
+  },
+  {
+    title: "Vikings",
+    days: 2,
+    hours: 17,
+    minutes:16
+  }
+];
+
+function calculatePercentageOfLife(days, hours, minutes, totalMinutesInLife) {
+  const seriesDurationMinutes = (days * 24 * 60) + (hours * 60) + minutes;
+  return (seriesDurationMinutes / totalMinutesInLife) * 100;
+}
+
+function logOutSeriesText() {
+  const averageLifespanYears = 80;
+  const totalMinutesInLife = averageLifespanYears * 365.25 * 24 * 60; // converting years to minutes
+  
+  let totalPercentage = 0;
+  seriesDurations.forEach(series => {
+    const percentageOfLife = calculatePercentageOfLife(series.days, series.hours, series.minutes, totalMinutesInLife);
+    totalPercentage += percentageOfLife;
+    console.log(`${series.title} took ${percentageOfLife.toFixed(3)}% of my life`);
+  });
+
+  console.log(`In total that is ${totalPercentage.toFixed(3)}% of my life`);
+}
+
+logOutSeriesText();

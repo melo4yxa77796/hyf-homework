f8e5b467de51ec8c9e4487df073000efbeef9ce8
melo4yxa77796 <g_julija_777@mail.ru>
2024-09-18 13:31:47 +0200

    homework node js week1

---
+++ nodejs:week1/avg.js
@@ -0,0 +1,22 @@
+
+const args = process.argv.slice(2);
+
+
+const calculateAverage = (numbers) => {
+  if (numbers.length === 0) return 'No numbers provided';
+
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  return (sum / numbers.length).toFixed(2); 
+};
+
+
+const numbers = args
+  .map(arg => parseFloat(arg)) 
+  .filter(num => !isNaN(num)); 
+
+if (numbers.length === 0) {
+  console.log('No valid numbers provided');
+} else {
+  const average = calculateAverage(numbers);
+  console.log(average);
+}

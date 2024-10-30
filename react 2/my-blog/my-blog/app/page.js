573049a71468f8e365c12b1c5173b2a3a0132b70
melo4yxa77796 <g_julija_777@mail.ru>
2024-10-30 15:50:10 +0100

    Dynamic Rendering with useEffect

--- react 2/my-blog/my-blog/app/page.js
+++ react 2/my-blog/my-blog/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import React from "react";
 import AstronomyPage from "./components/AstronomyPictureOfTheDay";
+import MarsRoverPhotos from "./components/MarsRoverPhotos";
 
 export default function Home()  {
   return (
     <div>
       <h1>My Blog</h1>
 <AstronomyPage />
+<MarsRoverPhotos />
     </div>
          
   );
